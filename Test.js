547e2452074e7702a9222ec8ee418154c0358150
Sondos-Alsalooli <s.a-95-a.s@hotmail.com>
2019-09-13 16:11:26 +0300

    Update Test.js

--- Test.js
+++ Test.js
@@ -38,16 +38,63 @@ document.write(x + ',' + y + ',' + z);
 */
 
 
-//day 5
-var x = 10;
+////day 5
+//var x = 10;
+//
+//document.write('Hello, this is my first challenge and I will do it ' + x + ' times. <br> I will do it, I will learn JavaScript.');
+//
+//
+//
+////Day 6
+//function تجربة(name)
+//{
+//  document.write( "مرحباَ " + name);
+//}
+//
+//تجربة("سندس");
 
-document.write('Hello, this is my first challenge and I will do it ' + x + ' times. <br> I will do it, I will learn JavaScript.');
+////Day 8
+//let c =10; const b = 9;
+//
+//
+//document.
+//write(c + b);
 
+//Day 9
+// to get randome number between 1 and 10
+//for(var i = 0; i< 10; i++)
+//{
+//var x = Math.random(); 
+//document.write(x + '<br>');
+//
+//var y = Math.round(x * 10) ;
+//document.write(y + '<br>');
+//
+// }
 
+//Day 10
+//var oldDateWithTime = new Date(2019, 8, 13, 15, 53, 30); 
+//var dateToday = new Date();
+//document.write(oldDateWithTime + '<br>');
+//document.write(dateToday + '<br>');
+//var elapsed = dateToday - oldDateWithTime; 
+//document.write(elapsed + '<br>');
+//document.write(oldDateWithTime.getFullYear()  + '<br>');
+//document.write(oldDateWithTime.getMonth()  + '<br>');
+//document.write(oldDateWithTime.getDate()  + '<br>');
+//document.write(oldDateWithTime.getDay()  + '<br>');
+//document.write(oldDateWithTime.getHours()  + '<br>');
+//document.write(oldDateWithTime.getUTCHours()   + '<br>');
+//document.write(oldDateWithTime.toString()  + '<br>');
+//document.write(oldDateWithTime.toUTCString()  + '<br>');
+//document.write(oldDateWithTime.toLocaleDateString()  + '<br>');
+//document.write(oldDateWithTime.toLocaleTimeString()  + '<br>');
+//document.write(oldDateWithTime.toISOString()  + '<br>');
+// 
 
-
-
-
+//Day 11+ Day 12
+//window.alert('Hello');
+//console.log('popup box is shown up');
 
 
 
