89d746be31e06bc154a2efc1da2eddea91016fc2
Sondos-Alsalooli <s.a-95-a.s@hotmail.com>
2019-09-13 14:38:11 +0300

    Day 5

---
+++ Test.js
@@ -0,0 +1,52 @@
+/*
+//day1 + day2
+document.write("Welcome to JS World");
+document.write("-----------------");
+
+const smallNumbers= [1,2,3,4,5];
+document.write(smallNumbers);
+document.write("-----------------");
+const GreatNumbers= [6,7,8,9,10];
+document.write(GreatNumbers);
+document.write("-----------------");
+
+//Combine 2 arrays using spread operator ...
+const NumbersUnder10 = [...smallNumbers, ...GreatNumbers];
+document.write(NumbersUnder10);
+
+
+//day3
+//variables ES5
+var a,b,c
+a = 5;
+b = 4;
+c = 6;
+a= 7;
+document.write('ES5 ');
+document.write(a + ',' + b + ',' + c);
+
+/*Varibles ES6*/
+/*var x = 10;
+
+const y = 4;
+let z = 6;
+
+document.write(' ES6 ');
+document.write(x + ',' + y + ',' + z);
+
+*/
+
+
+//day 5
+var x = 10;
+
+document.write('Hello, this is my first challenge and I will do it ' + x + ' times. <br> I will do it, I will learn JavaScript.');
+
+
+
+
+
+
+
+
+
